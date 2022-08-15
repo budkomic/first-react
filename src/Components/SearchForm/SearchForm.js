ee1b85bc3988bae74a074891c6835dc216bb0a8e
Michal Budkowski <michalbudkowski88@gmail.com>
2022-08-15 18:20:22 +0200

    Create TextInput

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -1,12 +1,13 @@
-import styles from './SearchForm.module.scss';
+import styles from './SearchForm.module.scss'
+import TextInput from '../TextInput/TextInput';
 
 const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
-            <input className={styles.input} type="text" />
+            <TextInput placeholder="Search..." />
             <button className={styles.button}>Search</button>
         </form>
     );
   };
 
-  export default SearchForm;
+export default SearchForm;
